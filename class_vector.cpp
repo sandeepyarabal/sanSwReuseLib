93f3cf4bb30c75a2f8b6ab2345e6bf440d4bfd97
sandeepyarabal <sandeep.yarabal@gmail.com>
2020-06-04 19:13:52 -0400

    Add files via upload

---
+++ class_vector.cpp
@@ -0,0 +1,45 @@
+// helloworld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+class vector
+{
+private:
+	double* elem;
+	int sz;
+public:
+	vector(int s)
+	{
+		sz = s;
+		elem = new double[s];
+	}
+	double& operator [](int i) { return elem[i]; };
+};
+
+int main()
+{
+	vector v(5);
+	
+	for (int i = 0; i < 5; i++)
+	{
+		v[i] = i;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << v[i] << ""<< "\n";
+	}
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
