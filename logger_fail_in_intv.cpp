b2ce40954037630cb4f87054c879f36aba225c3b
Sandeep Patil <sandeep.yarabal@gmail.com>
2021-12-16 22:11:59 -0500

    Create logger_fail_in_intv.cpp

---
+++ logger_fail_in_intv.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <memory>
+
+using namespace std;
+void unthreadsafecode(char* s)
+{
+   printf(" %s",s);
+}
+class logger
+{
+    public:
+    logger()
+    {
+        
+    }
+   ~logger()
+    {
+        
+    }
+    void threadsafecode(string s)
+    {
+        int n = s.length();
+        char char_array[n + 1];
+        strcpy(char_array, s.c_str());
+
+        unthreadsafecode(char_array);
+    }
+};
+
+int main()
+{
+    string Name = "sandeeppatil";
+
+    shared_ptr<logger> shlgr;
+    shlgr->threadsafecode(Name);
+    cout<<"Hello World";
+
+    return 0;
+    
+}
